a0167ddd7dacf0f5478b02847b2d047a720a0cf8
muemura <masashi.uemura@gunosy.com>
2024-04-07 15:03:43 +0900

    fix eslint config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { crx, defineManifest } from "@crxjs/vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 import packageJson from "./package.json" assert { type: "json" };
 
 export default defineConfig(() => {
