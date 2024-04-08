e804fc91098807b7916f4932449528aac92788d8
muemura <masashi.uemura@gunosy.com>
2024-04-08 22:36:28 +0900

    fix config

---
+++ src/sidepanel/SidePanel.tsx
@@ -0,0 +1,10 @@
+const SidePanel = () => {
+  return (
+    <div>
+      <h1>SidePanel</h1>
+      <p>This is a side panel that can be opened from the browser action.</p>
+    </div>
+  );
+};
+
+export default SidePanel;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { crx, defineManifest } from "@crxjs/vite-plugin";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 import packageJson from "./package.json" assert { type: "json" };
+
+const rootDir = resolve(__dirname);
+const srcDir = resolve(rootDir, "src");
 
 export default defineConfig(() => {
   const manifest = defineManifest({
@@ -14,28 +18,45 @@ export default defineConfig(() => {
     permissions: ["sidePanel", "tabs", "activeTab", "storage"],
     host_permissions: ["<all_urls>"],
     side_panel: {
-      default_path: "src/side_panel.tsx",
+      default_path: "src/sidepanel/index.html",
+    },
+    background: {
+      service_worker: "src/background/index.ts",
+      type: "module",
     },
   });
 
   return {
+    resolve: {
+      alias: [
+        { find: "@root", replacement: rootDir },
+        { find: "@src", replacement: srcDir },
+      ],
+    },
     plugins: [react(), crx({ manifest })],
+    minify: false,
+    modulePreload: false,
+    build: {
+      rollupOptions: {
+        input: {
+          sidepanel: resolve(srcDir, "sidepanel", "index.html"),
+          background: resolve(srcDir, "background", "index.ts"),
+        },
+        output: {
+          entryFileNames: "src/[name]/index.js",
+        },
+        external: ["webextension-polyfill"],
+      },
+    },
+    server: {
+      port: 25174,
+      strictPort: true,
+      hmr: {
+        port: 25174,
+      },
+    },
     define: {
       __APP_VERSION__: JSON.stringify(packageJson.version),
     },
   };
 });
-
-// export default defineConfig({
-//   plugins: [react(), crx({ manifest })],
-//   build: {
-//     rollupOptions: {
-//       input: {
-//         side_panel: "src/sidepanel.tsx",
-//       },
-//     },
-//   },
-//   define: {
-//     __APP_VERSION__: JSON.stringify(packageJson.version),
-//   },
-// });
